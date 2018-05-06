d090d880aa92a9a5bc42e3cffa967ace96928034
yin <>
2018-05-06 09:54:34 +0800

    拆分sb

---
+++ DYZB/DYZB/Home/View/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  DYZB
+//
+//  Created by linxiaoyu on 2018/5/5.
+//  Copyright © 2018年 yi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extention HomeViewController {
+    
+}
