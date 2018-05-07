a5b00f6239146bb2dbfd505017773d107cbf3c5e
yin <>
2018-05-07 23:17:30 +0800

    开始网络请求，使用pod

---
+++ DYZB/DYZB/NetworkTool.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkTool.swift
+//  DYZB
+//
+//  Created by linxiaoyu on 2018/5/7.
+//  Copyright © 2018年 yi. All rights reserved.
+//
+
+import UIKit
+
+enum MethodType {
+    case GET
+    case POST
+}
+
+class NetworkTool {
+    
+}
